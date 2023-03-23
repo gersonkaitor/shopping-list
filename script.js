f66023a5b11d86b4e6399da95773d2dbaeaf9794
gersonkaitor <gersonkaitor@gmail.com>
2023-03-23 14:33:47 +0800

    Remove & Clear Items

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
 
 const addItem = (e) => {
   e.preventDefault();
@@ -42,5 +43,19 @@ const createIcon = (classes) => {
   return icon;
 };
 
+const removeItem = (e) => {
+  if (e.target.parentElement.classList.contains('remove-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
+};
+
+const clearItems = (e) => {
+  while (itemList.firstChild) {
+    itemList.removeChild(itemList.firstChild);
+  }
+};
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearBtn.addEventListener('click', clearItems);
